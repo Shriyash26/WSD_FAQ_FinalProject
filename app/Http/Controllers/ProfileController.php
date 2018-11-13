2c546c09a9663a22b6e0549ae7f245036f686c73
shri <sm2689@njit.edu>
2018-11-13 16:16:37 -0500

    Task: added edit, update profile functions and route for edit profile, update profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -76,9 +76,12 @@ class ProfileController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit($user, $profile)
     {
-        //
+        $user = User::find($user);
+        $profile = $user->profile;
+        $edit = TRUE;
+        return view('profileForm', ['profile' => $profile, 'edit' => $edit ]);
     }
 
     /**
@@ -88,9 +91,21 @@ class ProfileController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request,  $user, $profile)
     {
-        //
+        $input = $request->validate([
+            'fname' => 'required',
+            'lname' => 'required',
+        ], [
+            'fname.required' => ' First is required',
+            'lname.required' => ' Last is required',
+        ]);
+        $profile = Profile::find($profile);
+        $profile->fname = $request->fname;
+        $profile->lname = $request->lname;
+        $profile->body = $request->body;
+        $profile->save();
+        return redirect()->route('home')->with('message', 'Updated Profile');
     }
 
     /**
