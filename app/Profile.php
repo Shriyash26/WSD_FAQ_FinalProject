92785c9640e8af7dc7b32a54cafaf68639a5a12b
shri <sm2689@njit.edu>
2018-11-13 15:54:33 -0500

    Task: added create, store profile functions and route for store profile

--- app/Profile.php
+++ app/Profile.php
@@ -6,8 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
+    protected $fillable = ['fname', 'lname', 'body'];
+
     public function user()
     {
+
         return $this->belongsTo('App\User');
     }
 }
