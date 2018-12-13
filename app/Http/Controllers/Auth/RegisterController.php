69963baf9d7c40729d1fab9af9af8fea0bf1ddc5
shri <sm2689@njit.edu>
2018-12-13 12:52:17 -0500

    Task: updated create user function to send email after registeration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use App\Mail\VerifyMail;
+use Illuminate\Support\Facades\Mail;
 
 class RegisterController extends Controller
 {
@@ -62,9 +64,13 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'token' => str_random(40)
         ]);
+        Mail::to($user->email)->send(new VerifyMail($user));
+
+        return $user;
     }
 }
